240ed42b9bf4dce66c844b540bdc59c91e330bca
Dzianis Katsuk <demis131194@gmail.com>
2021-04-02 12:20:23 +0300

    7 2 HW6 mail history

--- services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServiceClient.java
+++ services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServiceClient.java
@@ -16,6 +16,8 @@ public class MailServiceClient {
                 new QName("http://mail.service.masterjava.javaops.ru/", "MailServiceImplService"));
 
         MailService mailService = service.getPort(MailService.class);
-        mailService.sendMail(ImmutableList.of(new Addressee("demis131194@mail.ru", null)), Collections.emptyList(), "Subject", "Body");
+        mailService.sendMail(ImmutableList.of(
+                new Addressee("demis131194@mail.ru", null),
+                new Addressee("Bad Email <bad_email.ru>")), Collections.emptyList(), "Subject", "Body");
     }
 }
