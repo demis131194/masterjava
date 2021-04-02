710c057a5967020c3de4fe88f430a6f82acd5b71
Dzianis Katsuk <demis131194@gmail.com>
2021-04-02 21:02:34 +0300

    7 6 publish customizedWSDL

--- services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServiceClient.java
+++ services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServiceClient.java
@@ -13,11 +13,10 @@ public class MailServiceClient {
     public static void main(String[] args) throws MalformedURLException {
         Service service = Service.create(
                 new URL("http://localhost:8080/mail/mailService?wsdl"),
-                new QName("http://mail.service.masterjava.javaops.ru/", "MailServiceImplService"));
+                new QName("http://mail.javaops.ru/", "MailServiceImplService"));
 
         MailService mailService = service.getPort(MailService.class);
         mailService.sendMail(ImmutableList.of(
-                new Addressee("demis131194@mail.ru", null),
-                new Addressee("Bad Email <bad_email.ru>")), Collections.emptyList(), "Subject", "Body");
+                new Addressee("demis131194@mail.ru", null)), Collections.emptyList(), "Subject", "Body");
     }
 }
